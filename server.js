1406d22fa3eae257b09b1fc4426c855a2c9c40c4
Dzmitry Alifer <dzmitry.alifer@gmail.com>
2022-02-19 18:50:10 +0100

    updates to deploy to heroku

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(__dirname + '/dist/f1belvezha'));
+
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname + '/dist/f1belvezha/index.html'));
+});
+
+app.listen(process.env.PORT || 8080);
