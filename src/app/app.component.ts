4581fc365688c43b12bcb556241b040d619af972
Dzmitry Alifer <dzmitry.alifer@gmail.com>
2022-02-19 18:56:48 +0100

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'f1belvezha';
+  title = 'F1 Belvezha';
+  underConstruction = 'under construction.....';
 }
