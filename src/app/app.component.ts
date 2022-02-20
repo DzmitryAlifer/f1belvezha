48fe111f0b94f1e02d5afb8a2bbcc24787c462a0
Dzmitry Alifer <dzmitry.alifer@gmail.com>
2022-02-20 09:00:58 +0100

    add node.js side

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { HttpClient } from  '@angular/common/http';
+
+interface User {
+  id: number;
+  firstName: string;
+  lastName: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'F1 Belvezha';
-  underConstruction = 'under construction.....';
+  underConstruction = 'under construction...';
+
+  usersResponse = this.http.get('http://localhost:3000/users', {headers: {'Access-Control-Allow-Origin': '*'}});
+  
+  constructor(private http: HttpClient) {}
+
+  getAllUsers() {
+    this.usersResponse.subscribe(response => {console.log(response);});
+  }
 }
