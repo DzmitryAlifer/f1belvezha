a6c586fe824545b57404c6ed36286b784dab31b8
Dzmitry Alifer <dzmitry.alifer@gmail.com>
2022-02-20 12:47:33 +0100

    1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,10 @@ export class AppComponent {
   underConstruction = 'under construction...';
 
   // usersResponse = this.http.get('http://localhost:3000/users', {headers: {'Access-Control-Allow-Origin': '*'}});
-  usersResponse = this.http.get('https://safe-crag-81937.herokuapp.com/users', {headers: {'Access-Control-Allow-Origin': '*'}});
+  usersResponse = this.http.get('https://safe-crag-81937.herokuapp.com/users', {headers: {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET, POST, PATCH, PUT, DELETE, OPTIONS',
+  }});
   
   constructor(private http: HttpClient) {}
 
