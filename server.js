c8cd6d82b705aeab2f01b2c38008537d521e8713
Dzmitry Alifer <dzmitry.alifer@gmail.com>
2022-02-20 09:54:23 +0100

    change config

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const port = process.env.PORT || 8080;
 
 app.use(express.static(__dirname + '/dist/f1belvezha'));
 
@@ -8,4 +9,6 @@ app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname + '/dist/f1belvezha/index.html'));
 });
 
-app.listen(process.env.PORT || 8080);
+app.listen(port, () => {
+    console.log(`App running on port ${port}.`);
+});
