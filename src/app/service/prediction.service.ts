c94093f9540e44fe47b201e4275bff9ac3aa8c43
Dzmitry Alifer <dzmitry.alifer@gmail.com>
2022-02-27 11:15:26 +0100

    extract uri to const

--- src/app/service/prediction.service.ts
+++ src/app/service/prediction.service.ts
@@ -4,6 +4,9 @@ import {Params, Prediction} from '../types';
 import {HttpService} from './http.service';
 
 
+const PREDICTION_API = '/prediction';
+
+
 @Injectable({providedIn: 'root'})
 export class PredictionService {
 
@@ -11,14 +14,14 @@ export class PredictionService {
 
   getPrediction(userId: number, round: number): Observable<Prediction> {
     const queryParams: Params = {userId: String(userId), round: String(round)};
-    return this.httpService.getByParams<Prediction>('/predict', queryParams);
+    return this.httpService.getByParams<Prediction>(PREDICTION_API, queryParams);
   }
 
   makePrediction(prediction: Prediction): Observable<Prediction> {
-    return this.httpService.post<Prediction>('/predict', prediction);
+    return this.httpService.post<Prediction>(PREDICTION_API, prediction);
   }
 
   updatePrediction(prediction: Prediction): Observable<Prediction> {
-    return this.httpService.put<Prediction>('/predict', prediction);
+    return this.httpService.put<Prediction>(PREDICTION_API, prediction);
   }
 }
