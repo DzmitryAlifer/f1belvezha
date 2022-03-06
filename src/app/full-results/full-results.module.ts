72748f12b065a8f4bad7394f58a7d4e3333a221b
Dzmitry Alifer <dzmitry.alifer@gmail.com>
2022-03-06 23:00:26 +0100

    add mat-button over mat-icon

--- src/app/full-results/full-results.module.ts
+++ src/app/full-results/full-results.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
 import {MatIconModule} from '@angular/material/icon';
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
@@ -15,6 +16,7 @@ import {fullResultsReducer} from './store/full-results.reducer';
 @NgModule({
   imports: [
     CommonModule,
+    MatButtonModule,
     MatCardModule,
     MatIconModule,
     MatProgressSpinnerModule,
