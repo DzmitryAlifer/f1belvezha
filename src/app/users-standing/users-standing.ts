68079dcc2f1994e4c0481926bc173cccb0c59e91
Dzmitry Alifer <dzmitry.alifer@gmail.com>
2022-03-07 19:51:01 +0100

    mark current user in standing table

--- src/app/users-standing/users-standing.ts
+++ src/app/users-standing/users-standing.ts
@@ -2,6 +2,7 @@ import {ChangeDetectionStrategy, Component} from '@angular/core';
 import {Store} from '@ngrx/store';
 import {map} from 'rxjs/operators'; 
 import * as fullResultsSelectors from '../full-results/store/full-results.selectors';
+import * as toolbarSelectors from '../toolbar/store/toolbar.selectors';
 
 
 @Component({
@@ -12,8 +13,11 @@ import * as fullResultsSelectors from '../full-results/store/full-results.select
 })
 export class UsersStandingComponent {
   readonly columns = ['place', 'name', 'points'];
+
+  readonly isDarkMode = this.store.select(toolbarSelectors.selectIsDarkMode);
   readonly users = this.store.select(fullResultsSelectors.selectUsers).pipe(
     map(users => [...users].sort((left, right) => (left.seasonpoints > right.seasonpoints) ? -1 : 1)));
+  readonly currentUser = this.store.select(toolbarSelectors.selectCurrentUser);
 
   constructor(private readonly store: Store) {}
 }
