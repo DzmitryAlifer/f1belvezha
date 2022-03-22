97634a6ccc98563f3e5721cb48eb98cbcf2729e0
Dzmitry Alifer <dzmitry.alifer@gmail.com>
2022-03-22 23:48:39 +0100

    1

--- src/app/service/news.service.ts
+++ src/app/service/news.service.ts
@@ -2,7 +2,7 @@ import {HttpClient} from '@angular/common/http';
 import {Injectable} from '@angular/core';
 import {Observable} from 'rxjs';
 
-const HOME = 'https://f1sport.herokuapp.com/';
+
 const AUTOSPORT_COM_RSS_URL = 'https://www.autosport.com/rss/f1/news/';
 
 
@@ -12,6 +12,6 @@ export class NewsService {
   constructor(private readonly httpClient: HttpClient) {}
 
   getNews(): Observable<Object> {
-    return this.httpClient.get(HOME + AUTOSPORT_COM_RSS_URL);
+    return this.httpClient.get(AUTOSPORT_COM_RSS_URL);
   }
 }
