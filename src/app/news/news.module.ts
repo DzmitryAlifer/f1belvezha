929c5a217992a72e95b9de13defbcd24369a28f3
Dzmitry Alifer <dzmitry.alifer@gmail.com>
2022-03-25 19:28:46 +0100

    add news auto-refresh

--- src/app/news/news.module.ts
+++ src/app/news/news.module.ts
@@ -1,8 +1,9 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {MatButtonModule} from '@angular/material/button'; 
+import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
 import {MatIconModule} from '@angular/material/icon';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
 import {MatTooltipModule} from '@angular/material/tooltip';
 import {NewsComponent} from './news';
 
@@ -13,6 +14,7 @@ import {NewsComponent} from './news';
     MatButtonModule,
     MatCardModule,
     MatIconModule,
+    MatSlideToggleModule,
     MatTooltipModule,
   ],
   declarations: [NewsComponent],
--- src/app/news/news.ts
+++ src/app/news/news.ts
@@ -1,7 +1,11 @@
 import {ChangeDetectionStrategy, Component} from '@angular/core';
-import {merge, ReplaySubject} from 'rxjs';
-import {switchMap} from 'rxjs/operators';
+import {MatSlideToggleChange} from '@angular/material/slide-toggle';
+import {BehaviorSubject, combineLatest, interval, merge, ReplaySubject} from 'rxjs';
+import {filter, switchMap} from 'rxjs/operators';
 import {NewsService} from '../service/news.service';
+
+
+const NEWS_AUTO_REFRESH_INTERVAL = 5 * 60 * 1000;
 
 
 @Component({
@@ -14,10 +18,15 @@ export class NewsComponent {
   readonly Date = Date;
 
   private readonly refreshNewsSubject = new ReplaySubject<void>(1);
+  private readonly toggleAutoRefreshSubject = new BehaviorSubject<boolean>(false);
+  private readonly autoRefreshNews = 
+      combineLatest([this.toggleAutoRefreshSubject, interval(NEWS_AUTO_REFRESH_INTERVAL)])
+          .pipe(filter(([checked]) => !!checked));
+  private readonly triggerNewsRefresh = merge(this.refreshNewsSubject, this.autoRefreshNews);
 
-  private readonly refreshedNewsEn = this.refreshNewsSubject.pipe(
+  private readonly refreshedNewsEn = this.triggerNewsRefresh.pipe(
     switchMap(() => this.newsService.getNewsEn()));
-  private readonly refreshedNewsRu = this.refreshNewsSubject.pipe(
+  private readonly refreshedNewsRu = this.triggerNewsRefresh.pipe(
     switchMap(() => this.newsService.getNewsRu()));
 
   readonly newsLeftList = merge(this.newsService.getNewsEn(), this.refreshedNewsEn);
@@ -27,6 +36,10 @@ export class NewsComponent {
 
   refreshNews(): void {
     this.refreshNewsSubject.next();
+  }
+
+  toggleAutoRefresh({checked}: MatSlideToggleChange): void {
+    this.toggleAutoRefreshSubject.next(checked);
   }
 
   addTargetBlankAttribute(newsText: string): string {
