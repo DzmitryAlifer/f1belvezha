1317e285dc7c959028a268e75fa0e009d5135550
Dzmitry Alifer <dzmitry.alifer@gmail.com>
2022-04-04 21:32:28 +0200

    add rules

--- src/app/game-rules/game-rules.ts
+++ src/app/game-rules/game-rules.ts
@@ -1,5 +1,5 @@
 import {ChangeDetectionStrategy, Component} from '@angular/core';
-import {DRIVER_IN_LIST_PTS, DRIVER_PLACE_PTS} from '../common';
+import {CORRECT_TEAM_FROM_PAIR_PTS, DRIVER_IN_LIST_PTS, DRIVER_PLACE_PTS, WRONG_TEAM_PTS} from '../common';
 
 
 @Component({
@@ -11,4 +11,6 @@ import {DRIVER_IN_LIST_PTS, DRIVER_PLACE_PTS} from '../common';
 export class GameRulesComponent {
   readonly DRIVER_IN_LIST_PTS = DRIVER_IN_LIST_PTS;
   readonly DRIVER_PLACE_PTS = DRIVER_PLACE_PTS;
+  readonly CORRECT_TEAM_FROM_PAIR_PTS = CORRECT_TEAM_FROM_PAIR_PTS;
+  readonly WRONG_TEAM_PTS = WRONG_TEAM_PTS
 }
