54f7f1477b3552d89cffd53e896bf4634aff5475
Dzmitry Alifer <dzmitry.alifer@gmail.com>
2022-05-08 08:52:54 +0200

    fix points calculation

--- src/app/service/prediction.service.ts
+++ src/app/service/prediction.service.ts
@@ -1,7 +1,7 @@
 import {Injectable} from '@angular/core';
 import {Observable} from 'rxjs';
 import {map} from 'rxjs/operators';
-import {findNextEvent2} from 'src/app/common';
+import {findNextEvent2, NOT_SELECTED_DRIVER_NAME} from 'src/app/common';
 import {Params, Prediction} from '../types';
 import {HttpService} from './http.service';
 
@@ -15,19 +15,33 @@ export class PredictionService {
   constructor(private readonly httpService: HttpService) {}
 
   getAllPredictions(): Observable<Prediction[]> {
-    return this.httpService.getAll<Prediction[]>(PREDICTION_API);
+    return this.httpService.getAll<Prediction[]>(PREDICTION_API).pipe(
+      map(predictions => predictions.map(prediction => ({
+        ...prediction,
+        qualification: prediction.qualification.filter(driverName => !!driverName && driverName !== NOT_SELECTED_DRIVER_NAME),
+        race: prediction.race.filter(driverName => !!driverName && driverName !== NOT_SELECTED_DRIVER_NAME),
+      }))));
   }
 
   getAllUserPredictions(userId: number): Observable<Prediction[]> {
     const queryParams: Params = {userId: String(userId)};
-    return this.httpService.getByParams<Prediction[]>(PREDICTION_API, queryParams);
+    return this.httpService.getByParams<Prediction[]>(PREDICTION_API, queryParams).pipe(
+      map(predictions => predictions.map(prediction => ({
+        ...prediction,
+        qualification: prediction.qualification.filter(driverName => !!driverName && driverName !== NOT_SELECTED_DRIVER_NAME),
+        race: prediction.race.filter(driverName => !!driverName && driverName !== NOT_SELECTED_DRIVER_NAME),
+      }))));
   }
 
   getPrediction(userId: number, round: number): Observable<Prediction> {
     const queryParams: Params = {userId: String(userId), round: String(round)};
     
     return this.httpService.getByParams<Prediction>(PREDICTION_API, queryParams).pipe(
-      map(predictions => predictions));
+      map(prediction => ({
+        ...prediction,
+        qualification: prediction.qualification.filter(driverName => !!driverName && driverName !== NOT_SELECTED_DRIVER_NAME),
+        race: prediction.race.filter(driverName => !!driverName && driverName !== NOT_SELECTED_DRIVER_NAME),
+      })));
   }
 
   makePrediction(prediction: Prediction): Observable<Prediction> {
